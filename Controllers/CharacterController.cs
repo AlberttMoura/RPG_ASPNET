c2ebec47d695131de08399216b7858ed5df9d694
AlberttMoura <alberrttmoura@gmail.com>
2023-05-31 15:25:27 -0300

    Creating Post method

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -22,5 +22,12 @@ namespace RPG.Controllers
         {
             return Ok(characters.FirstOrDefault(c => c.Id == id));
         }
+
+        [HttpPost]
+        public ActionResult<List<Character>> AddCharacter(Character newCharacter)
+        {
+            characters.Add(newCharacter);
+            return Ok(characters);
+        }
     }
 }
