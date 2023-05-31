0aa9e9cb6bfbe904a2f33c88033821f080e44d1d
AlberttMoura <alberrttmoura@gmail.com>
2023-05-31 14:19:12 -0300

    Creating first models

---
+++ Models/Character.cs
@@ -0,0 +1,18 @@
+namespace RPG.Models
+{
+
+    public class Character
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; } = "Knight";
+
+        public int HitPoints { get; set; } = 10;
+
+        public int Strength { get; set; } = 14;
+
+        public int Defense { get; set; } = 12;
+
+        public RpgClass Class { get; set; } = RpgClass.Knight;
+    }
+}
---
+++ Models/RpgClass.cs
@@ -0,0 +1,12 @@
+namespace RPG.Models
+{
+    public enum RpgClass
+    {
+        Knight = 1,
+        Mage = 2,
+        Cleric = 3,
+        Rogue = 4,
+        Barbarian = 5,
+        Ranger = 6
+    }
+}
